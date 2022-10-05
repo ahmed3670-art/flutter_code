38324e4b9c84e9909bd90a5032abd467cb38551b
Ahmed Aly <57324624+ahmed3670-art@users.noreply.github.com>
2022-10-05 03:38:28 +0200

    Delete weather directory

--- weather/lib/Core/utiles/Const.dart
+++
@@ -1,5 +0,0 @@
-class Constatint {
-  static const String BaseUrl =
-      "https://api.openweathermap.org/data/2.5/weather";
-  static const String apikey = "4f086a7c66ebdbecb6f2dc0ebbf5413c";
-}
--- weather/lib/Domian/Repository/Base_wether_reposetoriy.dart
+++
@@ -1,5 +0,0 @@
-import 'package:weather/Domian/Entities/weather.dart';
-
-abstract class BaseWetherReposetoriy {
-  Future<Weather> getWeatherByCityName(String CityName);
-}
